95e4dccc0818abca548fd338803a8c03eaf81fa4
Dellrodar <3357168+Dellrodar@users.noreply.github.com>
2020-06-11 00:14:57 -0400

    First Tests added * Added tests for inputing to first 3 fields. * Added tests for submitting

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,44 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor, queryByText } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm';
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test('submits correct values', async () => {
+  const { container } = render(<ContactForm />)
+  const firstName = container.querySelector('input[name="firstName"]');
+  const lastName = container.querySelector('input[name="lastName"]');
+  const email = container.querySelector('input[name="email"]');
+  const submit = container.querySelector('input[type="submit"]');
+
+  await waitFor(() => {
+    fireEvent.change(firstName, {
+      target: {
+        value: "mockname"
+      }
+    })
+  })
+
+  await waitFor(() => {
+    fireEvent.change(lastName, {
+      target: {
+        value: "Henderson"
+      }
+    })
+  })
+
+  await waitFor(() => {
+    fireEvent.change(email, {
+      target: {
+        value: "mock@email.com"
+      }
+    })
+  })
+
+  await waitFor(() => {
+    fireEvent.click(submit)
+  })
+})
